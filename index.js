0d644e9aa333b419052dd7a5592cd3dc3bb30a75
Jesus Esquer <jme2791@gmail.com>
2020-07-18 20:07:06 -0700

    get image and set it in modal

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 function Gallery(gallery) {
+  if (!gallery) {
+    throw new Error(" No Gallery found");
+  }
+  // Select all images from index file
+  const images = Array.from(gallery.querySelectorAll("img"));
+  const modal = document.querySelector(".modal");
+  const prevButton = modal.querySelector(".prev");
+  const nextButton = modal.querySelector(".next");
 
-    const buttons = gallery.querySelectorAll('button')
-    function showNextImage() {
-        
+  function showImage(el) {
+    if (!el) {
+      console.info("no image to show");
+      return;
     }
+    // update modul with info
+    console.log(el);
+    modal.querySelector("img").src = el.src;
+    modal.querySelector("h2").textContent = el.title;
+    modal.querySelector("figure p").textContent = el.dataset.description;
+    currentImage = el;
+  }
+
+  images.forEach((image) =>
+    image.addEventListener("click", (e) => showImage(e.currentTarget))
+  );
+  console.log(images);
 }
+
+const gallery1 = Gallery(document.querySelector(".gallery1"));
+const gallery2 = Gallery(document.querySelector(".gallery2"));
